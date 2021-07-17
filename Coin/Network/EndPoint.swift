3b006bd900c6a4ff55b58d32a630803880199e70
hoonha choi <sosqsz2@naver.com>
2021-07-18 02:16:54 +0900

    refactor: krw 원화만 검색 되도록 Endpoint 수정 #1
    - 백엔드와 협의 후 btc , ustd는 검색되지 않도록 변경

--- Coin/Network/EndPoint.swift
+++ Coin/Network/EndPoint.swift
@@ -14,7 +14,10 @@ enum EndPoint {
         component.host = "34.64.77.122"
         component.port = 8080
         component.path = "/api/v1/tickers"
-        component.queryItems = [URLQueryItem(name: "search", value: keyword)]
+        component.queryItems = [
+            URLQueryItem(name: "search", value: keyword),
+            URLQueryItem(name: "market", value: "krw")
+        ]
         return component.url
     }
 }

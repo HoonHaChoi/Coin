4816c0c3e1ed2c568a8e3e97300cc27e45ad8621
hoonha choi <sosqsz2@naver.com>
2021-07-19 22:35:40 +0900

    feat: socket io 라이브러리 추가 및 소켓연결 구현 #3

--- Coin/ViewController.swift
+++ Coin/ViewController.swift
@@ -1,15 +1,36 @@
 import UIKit
 import Combine
+import SocketIO
 
 class ViewController: UIViewController, Storyboarded {
+  
+    let manager =  SocketManager(socketURL: URL(string: "http://34.64.77.122:8080/socket")!,
+                                 config: [.log(true),
+                                          .forceWebsockets(true),
+                                          .path("/socket")])
+    
+    lazy var socket:SocketIOClient = manager.defaultSocket
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        connectSocket()
     }
     
     var coordinator: MainCoordinator?
     
     @IBAction func moveSearchViewController(_ sender: Any) {
         coordinator?.showSearchViewController()
+    }
+    
+    func connectSocket() {
+        socket.on("message") { data, ack in
+            print("message",data,ack)
+        }
+    
+        socket.on("tickers") { data, ack in
+            print("tickers",data,ack)
+        }
+        
+        socket.connect()
     }
 }

9096c2164e3c60e80eb9fceb4a8bb09ab099436d
hoonha choi <sosqsz2@naver.com>
2021-07-20 15:43:39 +0900

    feat: 소켓 데이터를 이용한 UI 구현, swiftJSON 라이브러리 추가
    - 소켓으로 오는 데이터는 매우 불편한 데이터 [Any] 를 가지고 있고 변환하려면 딕셔너리로 하나하나 접근해야하는 단점이 있어
    라이브러리를 사용해 Codable 하도록 작성했다

--- Coin/ViewController.swift
+++ Coin/ViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Combine
 import SocketIO
+import SwiftyJSON
 
 class ViewController: UIViewController, Storyboarded {
   
@@ -13,8 +14,19 @@ class ViewController: UIViewController, Storyboarded {
     
     @IBOutlet weak var tableView: UITableView!
     
+    private let imageLoader = ImageLoader()
+    private var coins: [Coin] = [] {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.register(cell: SearchCoinCell.self)
+        tableView.rowHeight = 80
         connectSocket()
     }
     
@@ -25,10 +37,29 @@ class ViewController: UIViewController, Storyboarded {
     }
     
     func connectSocket() {
+        let decode = JSONDecoder()
+        decode.keyDecodingStrategy = .convertFromSnakeCase
+            
         socket.on("tickers") { data, ack in
-            print("tickers",data)
+            let data = try? JSON(data[0]).rawData()
+            let codabledata = try? decode.decode([Coin].self, from: data ?? Data())
+            self.coins = codabledata ?? []
         }
         
         socket.connect()
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return coins.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchCoinCell.reuseIdentifier, for: indexPath) as? SearchCoinCell else {
+            return .init()
+        }
+        cell.configure(coin: coins[indexPath.row], imageLoader: imageLoader)
+        return cell
     }
 }

8df27f5cdb583ad008a83b096e3b82c2bad95ced
hoonha choi <sosqsz2@naver.com>
2021-07-27 04:39:33 +0900

    feat: EndPoint enum -> struct 객체로 변경, URLGenerator 프로토콜 추가 #5
    - 테스트 가능하도록 변경
    - 정적이 아닌 객체 형식을 가지도록 함

--- Coin/Network/EndPoint.swift
+++ Coin/Network/EndPoint.swift
@@ -7,17 +7,49 @@
 
 import Foundation
 
-enum EndPoint {
-    static func searchURL(keyword: String) -> URL? {
+enum URLRouter {
+    case search
+    case socket
+}
+
+protocol URLGenerator {
+    func url(path: URLRouter, keyword: String?) -> URL?
+}
+
+struct EndPoint: URLGenerator {
+    
+    func url(path: URLRouter, keyword: String? = nil) -> URL? {
+        switch path {
+        case .search:
+            return searchURL(keyword: keyword ?? "")
+        case .socket:
+            return socketURL
+        }
+    }
+    
+    private let scheme = "http"
+    private let host = "34.64.77.122"
+    private let port = 8080
+    
+    private func searchURL(keyword: String) -> URL? {
         var component = URLComponents()
-        component.scheme = "http"
-        component.host = "34.64.77.122"
-        component.port = 8080
+        component.scheme = scheme
+        component.host = host
+        component.port = port
         component.path = "/api/v1/tickers"
         component.queryItems = [
             URLQueryItem(name: "search", value: keyword),
             URLQueryItem(name: "market", value: "krw")
         ]
+        return component.url
+    }
+    
+    private var socketURL: URL? {
+        var component = URLComponents()
+        component.scheme = scheme
+        component.host = host
+        component.port = port
+        component.path = "/socket"
         return component.url
     }
 }

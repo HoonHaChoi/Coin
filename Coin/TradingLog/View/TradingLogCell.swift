eed2fbbcaca05ca40617cf33475f485d1539ab75
hoonha choi <sosqsz2@naver.com>
2021-08-01 22:34:07 +0900

    feat:  테이블 헤더 UI 구현 ,테이블 정렬 아이콘 추가  #14

---
+++ Coin/TradingLog/View/TradingLogCell.swift
@@ -0,0 +1,20 @@
+//
+//  TradingLogCell.swift
+//  Coin
+//
+//  Created by HOONHA CHOI on 2021/08/01.
+//
+
+import UIKit
+
+class TradingLogCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}

a20df4200c674865dc835a661e932163b64b600e
hoonha choi <sosqsz2@naver.com>
2021-08-03 03:39:40 +0900

    feat: TradingLogViewModel 생성 및 메소드 추가  #18

---
+++ Coin/TradingLog/TradingLogViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  TradingLogViewModel.swift
+//  Coin
+//
+//  Created by HOONHA CHOI on 2021/08/03.
+//
+
+import Foundation
+
+struct TradingLogViewModel {
+    
+    private var storage: CoreDataStorage
+    
+    init(storage: CoreDataStorage) {
+        self.storage = storage
+    }
+    
+    func fetchLog() -> [TradingLogMO] {
+        return storage.fetch()
+    }
+    
+    func insertLog(start: Int, end: Int, date: Date) {
+        storage.insert(start: start, end: end, date: date)
+    }
+    
+    func updateLog(index: Int, start: Int,
+                   end: Int) {
+        storage.update(index: index, start: start, end: end)
+    }
+    
+    func deleteLog(index: Int) {
+        storage.delete(index: index)
+    }
+}

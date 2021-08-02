bb0b026ace6ef4dbe4ab123d4807540fa531de0f
hoonha choi <sosqsz2@naver.com>
2021-08-03 03:24:48 +0900

    feat: Cell UI IBOutlet 등록 및 configure 함수 추가 #17

--- Coin/TradingLog/View/TradingLogCell.swift
+++ Coin/TradingLog/View/TradingLogCell.swift
@@ -9,12 +9,31 @@ import UIKit
 
 class TradingLogCell: UITableViewCell {
 
+    @IBOutlet weak var logStateIView: UIView!
+    @IBOutlet weak var numberOfDaysLabel: UILabel!
+    @IBOutlet weak var dayOfTheWeekLabel: UIStackView!
+    @IBOutlet weak var startPriceLabel: UILabel!
+    @IBOutlet weak var endPriceLabel: UILabel!
+    @IBOutlet weak var proceedsLabel: UILabel!
+    @IBOutlet weak var yieldLabel: UILabel!
+    @IBOutlet weak var memoLabel: UILabel!
+    @IBOutlet weak var memoStackView: UIStackView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    func configure(log: TradingLogMO) {
+        
+        self.startPriceLabel.text = "\(log.startPrice)"
+        self.endPriceLabel.text = "\(log.endPrice)"
+        self.proceedsLabel.text = "\(log.profit)"
+        self.yieldLabel.text = "\(log.rate)"
     }
 
 }

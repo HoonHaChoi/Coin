8a867d5852c37f18c02015e033b0e6501081a6f8
hoonha choi <sosqsz2@naver.com>
2021-08-05 19:07:34 +0900

    feat: TradingLogAddViewController 추가 및 날짜텍스트필드 inputView 데이터피커 추가 #25

---
+++ Coin/TradingLog/TradingLogAddViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TradingLogAddViewController.swift
+//  Coin
+//
+//  Created by HOONHA CHOI on 2021/08/05.
+//
+
+import UIKit
+
+class TradingLogAddViewController: UIViewController, Storyboarded {
+
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var startAmountTextField: UITextField!
+    @IBOutlet weak var endAmountTextField: UITextField!
+    @IBOutlet weak var memoTextView: UITextView!
+
+    private lazy var datePicker: UIDatePicker = {
+        var picker = UIDatePicker()
+        picker.preferredDatePickerStyle = .wheels
+        picker.datePickerMode = .date
+        picker.maximumDate = Date()
+        picker.locale = Locale(identifier: "ko_kr")
+        picker.minimumDate = Calendar.current.date(byAdding: .year,
+                                                   value: -10,
+                                                   to: Date())
+        return picker
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setDatePickerTextField()
+    }
+    
+    private func setDatePickerTextField() {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+       
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneBarButtonPressed))
+        toolbar.setItems([doneButton], animated: true)
+        
+        dateTextField.inputAccessoryView = toolbar
+        dateTextField.inputView = datePicker
+    }
+    
+    @objc private func doneBarButtonPressed() {
+        dateTextField.text = "\(datePicker.date)"
+    }
+}

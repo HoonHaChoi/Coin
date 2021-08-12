b04c3b16ca5c3fe216748b75e95844a17f5ad179
hoonha choi <sosqsz2@naver.com>
2021-08-13 03:54:03 +0900

    feat: topContainerView 추가, segment Control UI 추가 #36

--- Coin/TradingLog/TradingLogContanierViewController.swift
+++ Coin/TradingLog/TradingLogContanierViewController.swift
@@ -21,10 +21,46 @@ class TradingLogContanierViewController: UIViewController, Storyboarded {
         fatalError()
     }
     
+    @IBOutlet weak var topContainerView: UIView!
     @IBOutlet weak var tradingLogContainerView: UIView!
+    private lazy var segmentControl: UISegmentedControl = {
+        let segment = UISegmentedControl()
+        segment.setBackgroundImage(UIImage(), for: .normal, barMetrics: .default)
+        segment.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+        
+        segment.insertSegment(withTitle: "일별 기록", at: 0, animated: true)
+        segment.insertSegment(withTitle: "기간별 기록", at: 1, animated: true)
+        
+        segment.selectedSegmentIndex = 0
+        segment.setTitleTextAttributes([
+            NSAttributedString.Key.foregroundColor: UIColor.middleGrayColor,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .regular)
+        ], for: .normal)
+        segment.setTitleTextAttributes([
+            NSAttributedString.Key.foregroundColor: UIColor.fallColor,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .bold)
+        ], for: .selected)
+        
+        segment.addTarget(self, action: #selector(changeSegmentedControlLinePosition), for: .valueChanged)
+        segment.translatesAutoresizingMaskIntoConstraints = false
+        return segment
+    }()
+    
+    private lazy var underLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .fallColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 3
+        return view
+    }()
+    
+    private lazy var leadingDistance: NSLayoutConstraint = {
+        return underLineView.leadingAnchor.constraint(equalTo: segmentControl.leadingAnchor)
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        headerViewConfigure()
         configure()
     }
     
@@ -33,10 +69,37 @@ class TradingLogContanierViewController: UIViewController, Storyboarded {
     }
 
     private func configure() {
-        
         tradingLogViewController.view.frame = tradingLogContainerView.bounds
         tradingLogContainerView.addSubview(tradingLogViewController.view)
         addChild(tradingLogViewController)
         tradingLogViewController.didMove(toParent: self)
+    }
+    
+    private func headerViewConfigure() {
+        
+        topContainerView.addSubview(segmentControl)
+        topContainerView.addSubview(underLineView)
+        
+        NSLayoutConstraint.activate([
+            segmentControl.topAnchor.constraint(equalTo: topContainerView.topAnchor),
+            segmentControl.leadingAnchor.constraint(equalTo: topContainerView.leadingAnchor),
+            segmentControl.centerYAnchor.constraint(equalTo: topContainerView.centerYAnchor),
+            segmentControl.centerXAnchor.constraint(equalTo: topContainerView.centerXAnchor),
+
+            underLineView.bottomAnchor.constraint(equalTo: segmentControl.bottomAnchor),
+            underLineView.heightAnchor.constraint(equalToConstant: 3),
+            leadingDistance,
+            underLineView.widthAnchor.constraint(equalTo: segmentControl.widthAnchor, multiplier: 1 / CGFloat(segmentControl.numberOfSegments))
+        ])
+    }
+    
+    @objc private func changeSegmentedControlLinePosition() {
+        let segmentIndex = CGFloat(segmentControl.selectedSegmentIndex)
+        let segmentWidth = segmentControl.frame.width / CGFloat(segmentControl.numberOfSegments)
+        let leadingDistance = segmentWidth * segmentIndex
+        UIView.animate(withDuration: 0.2, animations: { [weak self] in
+            self?.leadingDistance.constant = leadingDistance
+            self?.view.layoutIfNeeded()
+        })
     }
 }

47952d24ef36069a3aa99e0c578991bb4ccdb5e9
hoonha choi <sosqsz2@naver.com>
2021-08-12 19:27:16 +0900

    feat: 메세지 알림 포그라운드, 백그라운드 delegate 추가 #35

--- Coin/AppDelegate.swift
+++ Coin/AppDelegate.swift
@@ -12,37 +12,50 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
         UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, _ in }
         application.registerForRemoteNotifications()
-        
-        
         // 현재 등록토큰 가져오기
-        Messaging.messaging().token { token, error in
-          if let error = error {
-            print("Error fetching FCM registration token: \(error)")
-          } else if let token = token {
-            print("FCM registration token: \(token)")
-            print("Remote FCM registration token: \(token)")
-          }
-        }
-        
+//        Messaging.messaging().token { token, error in
+//          if let error = error {
+//            print("Error fetching FCM registration token: \(error)")
+//          } else if let token = token {
+//            print("FCM registration token: \(token)")
+//            print("Remote FCM registration token: \(token)")
+//          }
+//        }
         return true
     }
     
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
         let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
         print("[Log] deviceToken :", deviceTokenString)
-          
+        // 디바이스 토큰 요청 성공시
         Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // MARK: Background Notification Receive
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult)
+                       -> Void) {
+      // If you are receiving a notification message while your app is in the background,
+      // this callback will not be fired till the user taps on the notification launching the application.
+      // TODO: Handle data of notification
+
+      // Print message ID.
+      if let messageID = userInfo["gcm.message_id"] {
+        print("Message ID: \(messageID)")
+      }
+      print(userInfo)
+      completionHandler(UIBackgroundFetchResult.newData)
     }
 }
 
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    
-}
-
 extension AppDelegate: MessagingDelegate {
+    // MARK: 토큰 갱신 모니터링
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-      print("Firebase registration token: \(String(describing: fcmToken))")
-
+//      print("Firebase registration token: \(String(describing: fcmToken))")
       let dataDict: [String: String] = ["token": fcmToken ?? ""]
       NotificationCenter.default.post(
         name: Notification.Name("FCMToken"),
@@ -52,4 +65,26 @@ extension AppDelegate: MessagingDelegate {
       // TODO: If necessary send token to application server.
       // Note: This callback is fired at each app startup and whenever a new token is generated.
     }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+  
+    // MARK: Foreground Notification Receive
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              willPresent notification: UNNotification,
+                              withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
+                                -> Void) {
+    let userInfo = notification.request.content.userInfo
+    print(userInfo)
+    completionHandler([[.badge, .sound, .banner]])
+  }
+
+    // MARK: Push Click Delegate
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              didReceive response: UNNotificationResponse,
+                              withCompletionHandler completionHandler: @escaping () -> Void) {
+    let userInfo = response.notification.request.content.userInfo
+    print(userInfo)
+    completionHandler()
+  }
 }

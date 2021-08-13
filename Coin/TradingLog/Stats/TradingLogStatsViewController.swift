4d26d70a3a85231a9643976a1ec22a1aad7f824b
hoonha choi <sosqsz2@naver.com>
2021-08-13 12:29:33 +0900

    feat: TradingLogStatsViewController class, storyboard 생성 #37

---
+++ Coin/TradingLog/Stats/TradingLogStatsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TradingLogStatsViewController.swift
+//  Coin
+//
+//  Created by HOONHA CHOI on 2021/08/13.
+//
+
+import UIKit
+
+final class TradingLogStatsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}

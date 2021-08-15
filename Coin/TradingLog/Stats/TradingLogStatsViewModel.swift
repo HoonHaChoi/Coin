a919b80c9445b634ec42c8ffcdf9fa7c2e0e12cb
hoonha choi <sosqsz2@naver.com>
2021-08-16 07:15:40 +0900

    refactor: StatsService -> StatsViewModel 변경 #39

--- Coin/TradingLog/Stats/TradingLogStatsViewModel.swift
+++ Coin/TradingLog/Stats/TradingLogStatsViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  TradingLogStatsViewModel.swift
+//  TradingLogStatsService.swift
 //  Coin
 //
 //  Created by HOONHA CHOI on 2021/08/16.
@@ -9,11 +9,36 @@ import Foundation
 
 struct TradingLogStatsViewModel {
     
-    private var statsState: TradingLogStatsDTO
-        
-    init(state: TradingLogStatsDTO = .empty) {
-        statsState = state
+    private let dateManager: DateManager
+    private let coreDataManager: CoreDataFetching
+    
+    init(dateManager: DateManager,
+         coreDataManager: CoreDataFetching) {
+        self.dateManager = dateManager
+        self.coreDataManager = coreDataManager
+    }
+
+    func moveNextMonth() {
+        dateManager.turnOfForward()
     }
     
+    func movePreviousMonth() {
+        dateManager.turnOfBackward()
+    }
+    
+    func fetch() -> TradingLogStatsDTO {
+        let logs = coreDataManager.fetchAscent(dates: dateManager.calculateMonthStartOfEnd())
+
+        guard let first = logs.first, let last = logs.last else {
+            return .empty
+        }
+        
+        return .init(stats: .init(startPrice: Int(first.startPrice),
+                                  endPrice: Int(last.endPrice),
+                                  logCount: logs.count),
+                     nextButtonState: dateManager.confirmNextMonth(),
+                     previousButtonState: dateManager.confirmPreviousMonth(),
+                     currentDateString: dateManager.currentDateString())
+    }
     
 }

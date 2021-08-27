9b3417e8ede87995f2d84651ed6fbd746e937e28
hoonha choi <sosqsz2@naver.com>
2021-08-27 13:31:52 +0900

    style: 거래소 테이블뷰 reloadRows 업데이트 코드 수정 #59

--- Coin/Main/Exchange/ExchangeViewController.swift
+++ Coin/Main/Exchange/ExchangeViewController.swift
@@ -100,18 +100,8 @@ class ExchangeViewController: UIViewController {
         dataSource.updateModel(indexes: findIndex, metaList: metaList)
         let indexPath = dataSource.makeIndexPath(indexes: findIndex)
         
-        cryptoView.cryptoTableView.performBatchUpdates({
-            DispatchQueue.main.async { [weak self] in
-                self?.cryptoView.cryptoTableView.reloadRows(at: indexPath, with: .none)
-                self?.updateChangeBackground(indexPaths: indexPath, changes: changes)
-            }
-        }) { _ in }
-    }
-    
-    private func updateChangeBackground(indexPaths: [IndexPath], changes: [Change]) {
-        for i in 0..<indexPaths.count {
-            self.cryptoView.cryptoTableView.cellForRow(at: indexPaths[i])?
-                    .updateBackgroundAnimation(change: changes[i])
+        DispatchQueue.main.async { [weak self] in
+            self?.cryptoView.reloadRows(at: indexPath, to: changes)
         }
     }
     

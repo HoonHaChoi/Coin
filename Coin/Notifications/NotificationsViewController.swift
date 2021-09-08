61ba6b76cc33396031c585778467ea19067c3c87
hoonha choi <sosqsz2@naver.com>
2021-09-09 06:28:58 +0900

    feat: addNotificationCryptoButton barbutton 추가 #72

--- Coin/Notifications/NotificationsViewController.swift
+++ Coin/Notifications/NotificationsViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class NotificationsViewController: UIViewController {
 
+    private lazy var addNotificationCryptoButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        button.tintColor = .basicColor
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        navigationItem.rightBarButtonItem = addNotificationCryptoButton
     }
 }

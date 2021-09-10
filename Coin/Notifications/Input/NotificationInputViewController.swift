36090e8ce93a8e0fc81aac0f05c38596e38fc2bb
hoonha choi <sosqsz2@naver.com>
2021-09-10 09:57:59 +0900

    feat: NotificaionInputView 생성 #73

---
+++ Coin/Notifications/Input/NotificationInputViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationInputViewController.swift
+//  Coin
+//
+//  Created by HOONHA CHOI on 2021/09/09.
+//
+
+import UIKit
+
+class NotificationInputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}

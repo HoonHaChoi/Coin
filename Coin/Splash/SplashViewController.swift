47e96304937ea7eefc4eda9080e6b675b39f167e
hoonha choi <sosqsz2@naver.com>
2021-10-04 21:54:53 +0900

    feat: 네트워크 체크 추가 #92

--- Coin/Splash/SplashViewController.swift
+++ Coin/Splash/SplashViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Lottie
+import Network
 
 final class SplashViewController: UIViewController {
 
@@ -16,10 +17,13 @@ final class SplashViewController: UIViewController {
     }()
     
     weak var coordinator: AppCoordinator?
+    private let moniter = NWPathMonitor()
+    private var isNetworkState: NWPath.Status = .unsatisfied
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        checkNetwork()
         configureAnimationView()
     }
     
@@ -29,9 +33,30 @@ final class SplashViewController: UIViewController {
         animationView.contentMode = .scaleAspectFill
         animationView.loopMode = .playOnce
         view.addSubview(animationView)
+        playAnimation()
+    }
+    
+    private func playAnimation() {
         animationView.play { [weak self] _ in
-            self?.coordinator?.showMainCoordinator()
+            if self?.isNetworkState == .satisfied {
+                self?.coordinator?.showMainCoordinator()
+            } else {
+                self?.failNetworkAlert()
+            }
         }
     }
     
+    private func checkNetwork() {
+        moniter.start(queue: DispatchQueue.global())
+        moniter.pathUpdateHandler = { path in
+            self.isNetworkState = path.status
+        }
+    }
+    
+    private func failNetworkAlert() {
+        let alert = UIAlertController(title: "",
+                                      message: "네트워크에 연결할 수 없습니다.\n 네트워크 상태 확인후 다시 시도해 주세요.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
 }

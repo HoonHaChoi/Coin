432818cb9b60ca575169acbcc1de0a6762338db6
hoonha choi <sosqsz2@naver.com>
2021-10-04 21:03:42 +0900

    feat: configureSplash 추가, SplashCoordinator 추가 #92

---
+++ Coin/Splash/SplashCoordinator.swift
@@ -0,0 +1,18 @@
+//
+//  SplashCoordinator.swift
+//  Coin
+//
+//  Created by HOONHA CHOI on 2021/10/04.
+//
+
+import UIKit
+
+final class SplashCoordinator: Coordinator {
+    
+    init() {
+    }
+    
+    func start() {
+        
+    }
+}
--- Coin/Splash/SplashViewController.swift
+++ Coin/Splash/SplashViewController.swift
@@ -10,7 +10,23 @@ import Lottie
 
 class SplashViewController: UIViewController {
 
+    private let animationView: AnimationView = {
+        let animationView = AnimationView(name: "Splash")
+        return animationView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureAnimationView()
     }
+    
+    private func configureAnimationView() {
+        animationView.frame = CGRect(x: 0, y: 0, width: 120, height: 120)
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFill
+        animationView.loopMode = .playOnce
+        view.addSubview(animationView)
+        animationView.play()
+    }
+    
 }

060847ab3ea9f7db4ae58d4399d42102a4fa1970
hoonha choi <sosqsz2@naver.com>
2021-10-23 16:00:27 +0900

    feat: NotificationInputServiceSpy 추가 #101

---
+++ CoinTests/Spy/NotificationInputServiceSpy.swift
@@ -0,0 +1,40 @@
+//
+//  NotificationInputServiceSpy.swift
+//  CoinTests
+//
+//  Created by HOONHA CHOI on 2021/10/23.
+//
+
+import Foundation
+import Combine
+
+final class NotificationInputServiceSpy: BaseSpy, NotificationInputService {
+     
+    var completeURL: String?
+    var completeHTTPMethod: HTTPMethod?
+    
+    func requestFavoriteCoins(uuidString: String) -> AnyPublisher<Coin, NetworkError> {
+        
+        return Future<Coin, NetworkError> { promise in
+            if self.isSuccess {
+                promise(.success(self.dummyModel.DummyCoin()))
+            } else {
+                promise(.failure(.invalidResponse))
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    func requestCompleteNotification(url: URL?, method: HTTPMethod, body: Data) -> AnyPublisher<Void, NetworkError> {
+        completeURL = url?.lastPathComponent
+        completeHTTPMethod = method
+        return Future<Void, NetworkError> { promise in
+            if self.isSuccess {
+                promise(.success(()))
+            } else {
+                promise(.failure(.invalidResponse))
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+    
+}

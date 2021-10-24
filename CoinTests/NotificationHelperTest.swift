5df6e485c9ab80d04b5d559f2631ad824f87bab4
hoonha choi <sosqsz2@naver.com>
2021-10-24 22:24:13 +0900

    feat: NotificationHelper Type,Cycle Mapping 테스트, TypeFindIndex 테스트 #101

--- CoinTests/NotificationHelperTest.swift
+++ CoinTests/NotificationHelperTest.swift
@@ -33,12 +33,53 @@ class NotificationHelperTest: XCTestCase {
         XCTAssertEqual(notificationHelper.cycleUUIDs.last, "fakeCycleUUID7")
     }
     
-    func test_WrongTypeName() {
+    func test_WrongTypeMapping() {
         let emptyTypeName = notificationHelper.mapping(typeName: "")
-        let wrongTypeName = notificationHelper.mapping(typeName: "test")
+        let wrongTypeName = notificationHelper.mapping(typeName: "wrong")
         
         XCTAssertTrue(emptyTypeName.isEmpty)
         XCTAssertTrue(wrongTypeName.isEmpty)
     }
+    
+    func test_CorrectTypeMapping() {
+        let correctUpTypeName = notificationHelper.mapping(typeName: "상승")
+        let correctDownTypeName = notificationHelper.mapping(typeName: "하락")
+        
+        XCTAssertFalse(correctUpTypeName.isEmpty)
+        XCTAssertEqual(correctUpTypeName, "up")
+        XCTAssertFalse(correctDownTypeName.isEmpty)
+        XCTAssertEqual(correctDownTypeName, "down")
+    }
 
+    func test_WrongCycleMapping() {
+        let emptyCycleName = notificationHelper.mapping(cycleName: "")
+        let wrongCycleName = notificationHelper.mapping(cycleName: "wrong")
+        
+        XCTAssertTrue(emptyCycleName.isEmpty)
+        XCTAssertTrue(wrongCycleName.isEmpty)
+    }
+    
+    func test_CorrectCycleMapping() {
+        let correctOneminuteiCycleName = notificationHelper.mapping(cycleName: "1분마다 알림")
+        let correctOneHourCycleName = notificationHelper.mapping(cycleName: "1시간마다 알림")
+        
+        XCTAssertEqual(correctOneminuteiCycleName, "fakeCycleUUID1")
+        XCTAssertEqual(correctOneHourCycleName, "fakeCycleUUID5")
+    }
+    
+    func test_WrongFindTypeIndex() {
+        let emptyTypeIndex = notificationHelper.findTypeIndex(type: "")
+        let wrongTypeIndex = notificationHelper.findTypeIndex(type: "wrong")
+        
+        XCTAssertEqual(emptyTypeIndex, 0)
+        XCTAssertEqual(wrongTypeIndex, 0)
+    }
+    
+    func test_CorrectFindTypeIndex() {
+        let downTypeIndex = notificationHelper.findTypeIndex(type: "down")
+        let upTypeIndex = notificationHelper.findTypeIndex(type: "up")
+        
+        XCTAssertEqual(downTypeIndex, 1)
+        XCTAssertEqual(upTypeIndex, 0)
+    }
 }

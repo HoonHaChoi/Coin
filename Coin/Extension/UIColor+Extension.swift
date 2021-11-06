b02534824f5cf1302c3fd08b1ec97b4739837a1c
hoonha choi <sosqsz2@naver.com>
2021-11-06 19:01:06 +0900

    feat: UIColor extension 기본값 추가 #103
    Library Test로 변경하여 테스트 진행 중이며, Asset에 컬러를 접근하지 못해 컬러값을 찾지 못해 강제 옵셔널을 제거하고 기본값을 지정

--- Coin/Extension/UIColor+Extension.swift
+++ Coin/Extension/UIColor+Extension.swift
@@ -11,37 +11,37 @@ extension UIColor {
     
     // MARK: ProFitBackgroundColor
     static var fallColor: UIColor {
-        return UIColor(named: "FALLColor")!
+        return UIColor(named: "FALLColor") ?? .init()
     }
     static var riseColor: UIColor {
-        return UIColor(named: "RISEColor")!
+        return UIColor(named: "RISEColor") ?? .init()
     }
     
     // MARK: FontColor
     static var basicColor: UIColor {
-        return UIColor(named: "191919")!
+        return UIColor(named: "191919") ?? .init()
     }
     static var middleGrayColor: UIColor {
-        return UIColor(named: "767676")!
+        return UIColor(named: "767676") ?? .init()
     }
     static var weakGrayColor: UIColor {
-        return UIColor(named: "999999")!
+        return UIColor(named: "999999") ?? .init()
     }
     
     // MARK: ViewBackgroundColor
     static var DEDEDE: UIColor {
-        return UIColor(named: "DEDEDE")!
+        return UIColor(named: "DEDEDE") ?? .init()
     }
     
     static var EDEDED: UIColor {
-        return UIColor(named: "EDEDED")!
+        return UIColor(named: "EDEDED") ?? .init()
     }
     
     static var statsBackground: UIColor {
-        return UIColor(named: "StatsBackground")!
+        return UIColor(named: "StatsBackground") ?? .init()
     }
     
     static var basicBackground: UIColor {
-        return UIColor(named: "BasicBackground")!
+        return UIColor(named: "BasicBackground") ?? .init()
     }
 }

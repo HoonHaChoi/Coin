49b4c90387ab2543a297922f04fe7c19d0be899e
hoonha choi <sosqsz2@naver.com>
2021-11-07 15:02:13 +0900

    feat: CurrentDateString, turnOfBackward 테스트 #104
    - 해당 메소드 실행이 예상한 값이 정확히 나오는지 테스트, 매개변수로 입력 받지 않아 예상 결과만 정확한지 테스트 가능

--- CoinTests/DateManagerTest.swift
+++ CoinTests/DateManagerTest.swift
@@ -6,16 +6,39 @@
 //
 
 import XCTest
+import Foundation
 
 class DateManagerTest: XCTestCase {
+    
+    var dateManger: DateManager!
 
     override func setUpWithError() throws {
+        dateManger = .init()
     }
 
     override func tearDownWithError() throws {
+        dateManger = nil
     }
 
-    func testExample() throws {
+    func test_CurrentDateString() throws {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy.MM"
+        
+        let expectationDateString = dateFormatter.string(from: .init())
+        let currentDateString = dateManger.currentDateString()
+        
+        XCTAssertEqual(currentDateString, expectationDateString)
     }
-
+    
+    func test_turnOfBackward() {
+        let calendar = Calendar.current
+        let expectationDate = calendar.date(byAdding: .month, value: -1, to: .init())
+        
+        dateManger.turnOfBackward()
+        
+        XCTAssertEqual(dateManger.currentDate.removeTimeStamp(),
+                       expectationDate?.removeTimeStamp())
+    }
+    
+    
 }
